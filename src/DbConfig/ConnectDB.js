b9b19672fa05e12753a43fc72d82b84518ef4995
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-12-02 16:19:25 +0530

    Task 11 Completed

--- src/DbConfig/ConnectDB.js
+++ src/DbConfig/ConnectDB.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("MongoDB connected successfully");
     } catch (error) {
         console.error("MongoDB connection failed:", error);
